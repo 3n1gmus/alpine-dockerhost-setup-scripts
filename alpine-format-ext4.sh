20bc69932214df7af97fc2fa5c17534cb57e9393
3n1gmus <wschoenly@tutanota.com>
2023-06-01 06:02:40 -0600

    updated to use parted

--- alpine-format-ext4.sh
+++ alpine-format-ext4.sh
@@ -1,27 +1,32 @@
-#!/bin/sh
-# Script to partition the whole drive, format it as ext4, and add it as a permanent mount at boot
+#!/bin/bash
+# Script to partition, format, and mount a disk as ext4
 
-# Specify the device and mount point
-DEVICE="/dev/sdb"   # Replace with your device name (without partition number)
-MOUNT_POINT="/mnt/new_volume"   # Replace with your desired mount point
+# Define the disk device name (e.g., /dev/sdb)
+DISK_DEVICE="/dev/sdb"
 
-# Partition the entire drive
-echo -e "g\nn\n\n\n\nw" | fdisk $DEVICE
+# Define the partition number (e.g., 1)
+PARTITION_NUMBER="1"
 
-# Format the partition with ext4
-PARTITION="${DEVICE}1"
-mkfs.ext4 $PARTITION
+# Define the mount point directory (e.g., /mnt/data)
+MOUNT_POINT="/mnt/data"
 
-# Create the mount point directory
-mkdir -p $MOUNT_POINT
+# Partition the disk
+sudo parted $DISK_DEVICE mklabel gpt
+sudo parted -a optimal $DISK_DEVICE mkpart primary ext4 0% 100%
 
-# Get the UUID of the formatted partition
-UUID=$(blkid -s UUID -o value $PARTITION)
+# Format the partition as ext4
+sudo mkfs.ext4 ${DISK_DEVICE}${PARTITION_NUMBER}
 
-# Add an entry to /etc/fstab for permanent mounting
-echo "UUID=$UUID $MOUNT_POINT ext4 defaults 0 0" >> /etc/fstab
+# Create the mount point directory if it doesn't exist
+sudo mkdir -p $MOUNT_POINT
+
+# Get the UUID of the partition
+UUID=$(sudo blkid -s UUID -o value ${DISK_DEVICE}${PARTITION_NUMBER})
+
+# Add a permanent mount entry to /etc/fstab
+echo "UUID=$UUID  $MOUNT_POINT  ext4  defaults  0  2" | sudo tee -a /etc/fstab
 
 # Mount the partition
-mount $MOUNT_POINT
+sudo mount -a
 
-echo "New volume has been partitioned, formatted as ext4, and mounted at $MOUNT_POINT."
+echo "Disk partitioned, formatted as ext4, and mounted successfully."
