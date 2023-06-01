abf9a14c322b80ff0665ac9c87f0c263683bae33
3n1gmus <wschoenly@tutanota.com>
2023-06-01 06:19:55 -0600

    updated to FDISK

--- alpine-format-ext4.sh
+++ alpine-format-ext4.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 # Script to partition, format, and mount a disk as ext4
 
-# Define the disk device name (e.g., /dev/sdb)
-DISK_DEVICE="/dev/sdb"
+# Set the device name of the disk to partition and format
+DEVICE="/dev/sdb"   # Replace with your desired device name
 
-# Define the partition number (e.g., 1)
-PARTITION_NUMBER="1"
+# Set the partition number to create
+PARTITION="${DEVICE}1"
 
-# Define the mount point directory (e.g., /mnt/data)
-MOUNT_POINT="/mnt/data"
-
-# Partition the disk
-sudo parted $DISK_DEVICE mklabel gpt
-sudo parted -a optimal $DISK_DEVICE mkpart primary ext4 0% 100%
+# Create a new partition
+echo -e "o\nn\np\n1\n\n\nw" | fdisk "${DEVICE}"
 
 # Format the partition as ext4
-sudo mkfs.ext4 ${DISK_DEVICE}${PARTITION_NUMBER}
+mkfs.ext4 "${PARTITION}"
 
-# Create the mount point directory if it doesn't exist
-sudo mkdir -p $MOUNT_POINT
+# Create a mount point directory
+MOUNT_POINT="/mnt/mydisk"   # Replace with your desired mount point
+mkdir -p "${MOUNT_POINT}"
 
-# Get the UUID of the partition
-UUID=$(sudo blkid -s UUID -o value ${DISK_DEVICE}${PARTITION_NUMBER})
-
-# Add a permanent mount entry to /etc/fstab
-echo "UUID=$UUID  $MOUNT_POINT  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+# Add an entry to /etc/fstab for permanent mounting
+echo -e "${PARTITION}\t${MOUNT_POINT}\text4\tdefaults\t0 0" >> /etc/fstab
 
 # Mount the partition
-sudo mount -a
+mount -a
 
-echo "Disk partitioned, formatted as ext4, and mounted successfully."
+echo "Disk partitioned, formatted as ext4, and permanently mounted at ${MOUNT_POINT}."
