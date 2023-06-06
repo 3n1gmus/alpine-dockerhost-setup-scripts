7833176ff5c8b837527b63469cdb72b1ed34eb2d
3n1gmus <31117809+3n1gmus@users.noreply.github.com>
2023-06-06 06:52:08 -0600

    Updated code

--- docker-proxy-setup.sh
+++ docker-proxy-setup.sh
@@ -1,3 +1,3 @@
 # creates network named 'proxy' that is bound to the host IP of the '--ip' setting
 
-docker network create --ip 192.168.1.5 proxy # Change ip address to match your system
+docker network create -o "com.docker.network.bridge.host_binding_ipv4"="192.168.1.20" proxy # Change ip address to match your system
