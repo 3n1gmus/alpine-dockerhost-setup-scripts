62c9155bd73e46f3b9753b105839ce6ea36f7308
3n1gmus <31117809+3n1gmus@users.noreply.github.com>
2023-06-07 15:37:00 -0600

    Updated with IP Listener setting.

--- configure-ssh.sh
+++ configure-ssh.sh
@@ -11,6 +11,10 @@ sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_
 sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 1800/' /etc/ssh/sshd_config
 sed -i 's/#ClientAliveCountMax 3/ClientAliveCountMax 0/' /etc/ssh/sshd_config
 
+# Update SSH IP address
+ip_address="192.168.1.33" # Replace with main IP Address
+# sed -i 's/#ListenAddress 0.0.0.0/ListenAddress ${ip_address}/' /etc/ssh/sshd_config
+
 # Restart the SSH service
 service sshd restart
 
