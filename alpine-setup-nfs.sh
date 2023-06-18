2ea02b4054ce2ba8cad630e1982d3ec235bfd4f4
3n1gmus <31117809+3n1gmus@users.noreply.github.com>
2023-06-18 07:26:25 -0600

    typo

--- alpine-setup-nfs.sh
+++ alpine-setup-nfs.sh
@@ -4,4 +4,4 @@ rc-update add nfsmount
 rc-service nfs start
 rc-service nfsmount start
 rc-status
-eacho "If nfs services are not started the system may need a reboot."
+echo "If nfs services are not started the system may need a reboot."
